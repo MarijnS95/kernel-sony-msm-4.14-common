8e2ae79b4deea8381790cd2dce4c20187b5a2b80
MarijnS95 <marijns95@gmail.com>
2019-12-12 12:08:40 +0100

    KEEPLOCAL: Switch gcc to Linaro 7.4.

--- build-kernels-gcc.sh
+++ build-kernels-gcc.sh
@@ -4,6 +4,8 @@
 
 # Cross Compiler
 CROSS_COMPILE=$ANDROID_ROOT/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+CROSS_COMPILE=/data/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
+
 # Build command
 BUILD_ARGS="CROSS_COMPILE=\"/usr/bin/ccache $CROSS_COMPILE\""
 
